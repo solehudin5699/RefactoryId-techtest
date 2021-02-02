c569f6b7272fa22c3ec94ec80dcde8b687b64d22
DELL-PC <solehudinamqi@yahoo.co.id>
2021-02-02 07:55:32 +0700

    update  soal1

---
+++ soal1/README.md
@@ -0,0 +1,30 @@
+### Soal Cerita
+
+Sebuah restoran ingin memiliki sebuah aplikasi untuk mencetak struk, restoran
+tersebut memiliki alat printer berjenis dot matriks. Dalam pengembangan aplikasi
+pencetak struk, kamu ditugaskan untuk mengolah data masukan dan keluaran agar
+sesuai dengan format yang bisa diprint oleh printer dengan ukuran kertas struk
+HVS.
+
+### Jawaban
+
+dengan menggunakan readline, maka input yang dinginkan untuk dicetak dapat
+dimasukkan pada terminal: Jalankan/run `node index` untuk menjalankannya Contoh
+tampilan pada terminal saat di running:
+
+```bash
+Tuliskan detail data yang akan dicetak...
+FORMAT->namawarung,tgl,kasir,produk1=harga1|produk2=harga2|produk3=harga3 dan seterusnya sejumlah produk yang dirder : Warung SederhaFORMAT->namawarung,tgl,kasir,produk1=harga1|produk2=harga2|produk3=harga3 dan seterusnya sejumlah produk yang dirder : Warung Sederhana,21/01/2021,Solehudin,Bakso=5000|Soto=7000|Mie Ayam=10000|Sate Ayam=50000|Perkedel=40000|Bakmi=10000
+       Warung Sederhana
+Tanggal :           21/01/2021
+Nama Kasir :         Solehudin
+==============================
+Bakso...................Rp5000
+Soto....................Rp7000
+Mie Ayam...............Rp10000
+Sate Ayam..............Rp50000
+Perkedel...............Rp40000
+Bakmi..................Rp10000
+
+Total.................Rp122000
+```
--- soal1/index.js
+++ soal1/index.js
@@ -1,83 +1,70 @@
-// const cetakStruk = (warung, tanggal, kasir) => {
-//   //Nama warung
-//   let rowWarung;
-//   if (warung.length < 30) {
-//     let space = 15 - Math.floor(warung.length / 2);
-//     rowWarung = " ".repeat(space) + warung;
-//   } else {
-//     rowWarung = warung.substring(0, 30);
-//   }
-//   //Tanggal
-//   let rowTanggal = `Tanggal : ${" ".repeat(
-//     20 - tanggal.length
-//   )}${tanggal.substring(0, 20)}`;
-//   //Nama Kasir
-//   let rowKasir = `Nama Kasir : ${" ".repeat(
-//     17 - kasir.length
-//   )}${kasir.substring(0, 17)}`;
-//   //Baris pemisah
-//   let rowPemisah = "=".repeat(30);
-//   console.log(rowWarung);
-//   console.log(rowTanggal);
-//   console.log(rowKasir);
-//   console.log(rowPemisah);
-// };
-// cetakStruk("Warung Makan Sederhana", "21/21/2020", "Jaya Bakti Polisi");
-// const readline = require("readline");
-// const rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-// });
-// let numOrder;
-// rl.on("line", (answer) => {
-//   numOrder = Number(answer);
-//   let products = [];
-//   for (let i = 1; i <= Number(numOrder); i++) {
-//     let nameProduct;
-//     rl.on("line", (answer) => {
-//       nameProduct = answer;
-//     });
-//     let priceProduct;
-//     rl.on("line", (answer) => {
-//       priceProduct = answer;
-//     });
-//     products.push({ nameProduct, priceProduct });
-//   }
-//   // rl.close();
-// });
 const readline = require("readline");
-const cetakStruk = async () => {
-  const rl = await readline.createInterface({
+
+const cetakIdentitas = (warung, tanggal, kasir) => {
+  //Nama warung
+  let rowWarung;
+  if (warung.length < 30) {
+    let space = 15 - Math.floor(warung.length / 2);
+    rowWarung = " ".repeat(space) + warung;
+  } else {
+    rowWarung = warung.substring(0, 30);
+  }
+  //Tanggal
+  let rowTanggal = `Tanggal : ${" ".repeat(
+    20 - tanggal.length
+  )}${tanggal.substring(0, 20)}`;
+  //Nama Kasir
+  let rowKasir = `Nama Kasir : ${" ".repeat(
+    17 - kasir.length
+  )}${kasir.substring(0, 17)}`;
+  //Baris pemisah
+  let rowPemisah = "=".repeat(30);
+  console.log(rowWarung);
+  console.log(rowTanggal);
+  console.log(rowKasir);
+  console.log(rowPemisah);
+};
+
+const cetakProduk = (produk) => {
+  produk.split("|").map((item) => {
+    let lengthDot =
+      30 - 2 - item.split("=")[0].length - item.split("=")[1].length;
+    let dots = ".".repeat(lengthDot);
+    let formatCetak = `${item.split("=")[0]}${dots}Rp${item.split("=")[1]}`;
+    return console.log(formatCetak);
+  });
+  let harga = produk.split("|").map((item) => item.split("=")[1]);
+  let hargaTotal = harga.reduce(
+    (total, value) => Number(total) + Number(value)
+  );
+  let formatCetakTotal = `Total${".".repeat(
+    30 - 7 - hargaTotal.toString().length
+  )}Rp${hargaTotal}`;
+  console.log("");
+  console.log(formatCetakTotal);
+};
+
+const cetakStruk = () => {
+  const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
-  let numOrder;
-  let a = await rl.question("What is your favorite food? ", (answer) => {
-    numOrder = answer;
-  });
-  // let products = [];
-  console.log(numOrder);
-  // for (let i = 1; i <= Number(numOrder); i++) {
-  //   rl.setPrompt(`Nama produk ${i} : `);
-  //   let nameProduct = rl.getPrompt();
-
-  //   rl.setPrompt(`Harga ${i} :`);
-  //   let priceProduct = rl.getPrompt();
-
-  //   let productOrdered = { nameProduct, priceProduct };
-  //   products.push(productOrdered);
-  // }
-  // let numOrder = prompt("Berapa jumlah jenis makanan yang diorder");
+  console.log("Tuliskan detail data yang akan dicetak...");
+  rl.question(
+    "FORMAT->namawarung,tgl,kasir,produk1=harga1|produk2=harga2|produk3=harga3 dan seterusnya sejumlah produk yang dirder : ",
+    (answer) => {
+      numOrder = answer;
+      //namawarung,tgl,kasir,product=harga|product=harga|product=harga
+      let namaWarung = answer.split(",")[0];
+      let tanggal = answer.split(",")[1];
+      let namaKasir = answer.split(",")[2];
+      let listProduk = answer.split(",")[3];
+      cetakIdentitas(namaWarung, tanggal, namaKasir);
+      cetakProduk(listProduk);
+      //cetakIdentitas()
+      // console.log(numOrder);
+      rl.close();
+    }
+  );
 };
 cetakStruk();
-
-// console.log("I will now ask you for your name.");
-// process.stdin.setEncoding("utf8");
-// var name;
-// process.stdin.on("readable", function () {
-//   name = process.stdin.read();
-//   if (name !== null) {
-//     console.log("Hello ".concat(name, ". How are you"));
-//     process.exit();
-//   }
-// });
